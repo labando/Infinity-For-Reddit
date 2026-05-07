405bc9b9a493459447c2189af2ef2e6ec4c45fae
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2026-05-07 16:09:37 -0400

    Start adding option to control the number of comment thread cache.

--- app/src/main/java/ml/docilealligator/infinityforreddit/settings/PostDetailsPreferenceFragment.kt
+++ app/src/main/java/ml/docilealligator/infinityforreddit/settings/PostDetailsPreferenceFragment.kt
@@ -1,18 +1,39 @@
-package ml.docilealligator.infinityforreddit.settings;
+package ml.docilealligator.infinityforreddit.settings
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
+import androidx.preference.Preference.SummaryProvider
+import ml.docilealligator.infinityforreddit.R
+import ml.docilealligator.infinityforreddit.customviews.preference.CustomFontPreferenceFragmentCompat
+import ml.docilealligator.infinityforreddit.utils.SharedPreferencesUtils
 
-import androidx.preference.PreferenceManager;
+class PostDetailsPreferenceFragment : CustomFontPreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        val preferenceManager = getPreferenceManager()
+        preferenceManager.setSharedPreferencesName(SharedPreferencesUtils.POST_DETAILS_SHARED_PREFERENCES_FILE)
+        setPreferencesFromResource(R.xml.post_details_preferences, rootKey)
 
-import ml.docilealligator.infinityforreddit.R;
-import ml.docilealligator.infinityforreddit.customviews.preference.CustomFontPreferenceFragmentCompat;
-import ml.docilealligator.infinityforreddit.utils.SharedPreferencesUtils;
+        val commentThreadContinuityCapacityEditTextPreference =
+            findPreference<EditTextPreference?>(SharedPreferencesUtils.COMMENT_THREAD_CONTINUITY_CAPACITY)
 
-public class PostDetailsPreferenceFragment extends CustomFontPreferenceFragmentCompat {
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        PreferenceManager preferenceManager = getPreferenceManager();
-        preferenceManager.setSharedPreferencesName(SharedPreferencesUtils.POST_DETAILS_SHARED_PREFERENCES_FILE);
-        setPreferencesFromResource(R.xml.post_details_preferences, rootKey);
+        if (commentThreadContinuityCapacityEditTextPreference != null) {
+            commentThreadContinuityCapacityEditTextPreference.onPreferenceChangeListener =
+                Preference.OnPreferenceChangeListener { _, newValue ->
+                    try {
+                        ((newValue as String?)?.toInt() ?: -1) >= 0
+                    } catch (_: NumberFormatException) {
+                        false
+                    }
+                }
+            commentThreadContinuityCapacityEditTextPreference.setSummaryProvider(
+                SummaryProvider { _: Preference? ->
+                    getString(
+                        R.string.settings_comment_thread_continuity_capacity_summary,
+                        commentThreadContinuityCapacityEditTextPreference.text
+                    )
+                }
+            )
+        }
     }
 }
